f303b09cf32130b6ec5fd55d926e022c1b9794de
aftermaaath <ella920711@gmail.com>
2022-11-23 08:44:55 +0800

    Lab5 q3q4 upd

--- Lab5/tb/Lab5_Team31_Booth_Multiplier_4bit_t.v
+++ Lab5/tb/Lab5_Team31_Booth_Multiplier_4bit_t.v
@@ -20,40 +20,39 @@ always#5 clk = ~clk;
 
 initial begin
     #10 rst_n = 1'b1;
-    #10 start = 1'b1;
-    #70 start = 1'b0;
+    #70 start = 1'b1;
     a = 4'b1000;
     b = 4'b0010;
-    #10 start = 1'b1;
-    #70 start = 1'b0;
+    #10 start = 1'b0;
+    #70 start = 1'b1;
     a = 4'b0010;
     b = 4'b1000;
-    #10 start = 1'b1;
-    #70 start = 1'b0;
+    #10 start = 1'b0;
+    #70 start = 1'b1;
     a = 4'b1101;
     b = 4'b0001;
-    #10 start = 1'b1;
-    #70 start = 1'b0;
+    #10 start = 1'b0;
+    #70 start = 1'b1;
     a = 4'b1000;
     b = 4'b1000;
-    #10 start = 1'b1;
-    #70 start = 1'b0;
+    #10 start = 1'b0;
+    #70 start = 1'b1;
     a = 4'b1000;
     b = 4'b0111;
-    #10 start = 1'b1;
-    #70 start = 1'b0;
+    #10 start = 1'b0;
+    #70 start = 1'b1;
     a = 4'b0000;
     b = 4'b0101;
-    #10 start = 1'b1;
-    #70 start = 1'b0;
+    #10 start = 1'b0;
+    #70 start = 1'b1;
     a = 4'b0000;
     b = 4'b1101;
-    #10 start = 1'b1;
-    #70 start = 1'b0;
+    #10 start = 1'b0;
+    #70 start = 1'b1;
     a = 4'b0010;
     b = 4'b0000;
-    #10 start = 1'b1;
-    #70 start = 1'b0;
+    #10 start = 1'b0;
+    #70 start = 1'b1;
     a = 4'b1010;
     b = 4'b0000;
 //    repeat(5)begin
@@ -62,7 +61,7 @@ initial begin
 //        a = $random;
 //        b = $random;
 //    end
-    #10 start = 1'b1;
+    #10 start = 1'b0;
     #70 $finish;
 end
 
